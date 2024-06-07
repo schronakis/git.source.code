9f04ae6e38f28256925f860f88b95b8fc16b37a1
schronakis <schronakis@gmail.com>
2024-06-07 10:47:15 +0300

    changes_in_db_connect

--- python.project/misc_code/db_connect.py
+++ python.project/misc_code/db_connect.py
@@ -10,48 +10,59 @@ class db_connect:
         self.trusted = trusted
 
         self.conn = pyodbc.connect(f'Driver={self.driver};Server={self.server};Database={self.database};Trusted_Connection={self.trusted};')
-
-    def db_commit(self):
-
-        return self.conn.commit()
         
     def initiate_connection(self):
 
         return self.conn.cursor()
-        
-    def end_connection(self):
 
-        return self.conn.close()
+my_work = db_connect( 'SQL Server', 'PW0B6TR5\MSSQL_2022', 'WorkTrain', 'yes') #10.249.32.152,1433\MSSQL_2022 ipaddress,port/sql_instance
+
+my_work_cursor = my_work.initiate_connection()
 
 
-##################### insert to db ######################
+##################### Insert into DataBase #####################
+my_work_cursor.execute("INSERT INTO dbo.client(client_id, birth_number, district_id) VALUES (?, ?, ?)", 12, 348909, 78)
+my_work_cursor.commit()
+################################################################
+
+###################### Read from DataBase ######################
+my_work_cursor.execute('SELECT * FROM dbo.client')
+
+columns = [column[0] for column in my_work_cursor.description]
+print(columns)
+
+for row in my_work_cursor:
+    # print(row)
+    row_to_list = [elem for elem in row]
+
+    print(row_to_list)
+################################################################
+
+my_work_cursor.close()
+
+##################### pyodbc implementation no class ######################
+###########################################################################
+############################## init connection ############################
+# driver ='SQL Server'
+# server = 'PW0B6TR5\MSSQL_2022'
+# database = 'WorkTrain'
+# trusted = 'yes'
+
+# conn = pyodbc.connect(f'Driver={driver};Server={server};Database={database};Trusted_Connection={trusted};')
 # cursor = conn.cursor()
-# cursor.execute("INSERT INTO HumanResources.Department(Name, GroupName, ModifiedDate) VALUES (?, ?, ?)", 'Access', 'AC', '2023-08-10 23:00:00.000')
+
+########################################## insert into db ##########################################
+# cursor.execute("INSERT INTO dbo.client(client_id, birth_number, district_id) VALUES (?, ?, ?)", 12, 348909, 78)
 
 # conn.commit()
-#########################################################
+###################################################################################################
 
-##################### select rom db #####################
-# cursor.execute('SELECT * FROM stg.accounts')
+########################################## select from db ##########################################
+# cursor.execute('SELECT * FROM dbo.client')
 
 # for row in cursor:
 #     # print(row)
 #     row_to_list = [elem for elem in row]
 
-# print(row_to_list)
-#########################################################
-
-# my_work = db_connect( 'SQL Server', 'E352012', 'MyWork', 'yes')
-
-# my_work_cursor = my_work.initiate_connection()
-
-# my_work_cursor.execute('SELECT * FROM stg.accounts')
-
-# columns = [column[0] for column in my_work_cursor.description]
-# print(columns)
-
-# for row in my_work_cursor:
-#     # print(row)
-#     row_to_list = [elem for elem in row]
-
-#     print(row_to_list)
+# print(row_to_list)  
+###################################################################################################
